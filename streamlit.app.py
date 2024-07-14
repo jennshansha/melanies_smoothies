6bc9d91c4f6c80ebcc3fa459b06fc38dd4e9aed3
Jennifer <jennshansha@users.noreply.github.com>
2024-07-14 11:39:52 -0500

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -41,7 +42,7 @@ if ingredients_list:
 
    #st.write(my_insert_stmt)
    #st.stop()
-   import requests 
+   
    time_to_insert = st.button('Submit Order')
 
    if time_to_insert:
